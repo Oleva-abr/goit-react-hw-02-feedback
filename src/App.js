ecd14eca9e90e5573292cad7d8d83717b840e475
Olena Abramova <olena.v.abramova@gmail.com>
2021-04-03 18:41:39 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import "./App.css";
+import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+        positivePercentage: 0,
+    };
+    countTotalFeedback = () => {};
+    countPositiveFeedbackPercentage = () => {};
+    onLeaveFeedback = () => {};
+    render() {}
 }
 
 export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,49 +1,62 @@
 import React from 'react';
 import propTypes from 'prop-types';
 class Statistics extends React.Component {
-  render() {
-    state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-      positivePercentage: 0,
-    };
-    return (
-      <>
-        <h1>Please leave feedback </h1>
-        <button>
-          Good: <span>{good}</span>
-        </button>
-        <button>
-          Bad: <span>{bad}</span>
-        </button>
-        <button>
-          Neutral: <span>{neutral}</span>
-        </button>
-        <h2>Statistics</h2>
-        <h3>
-          Good: <span>{good}</span>
-        </h3>
-        <h3>
-          Bad: <span>{bad}</span>
-        </h3>
-        <h3>
-          Neutral: <span>{neutral}</span>
-        </h3>
-        <h3>
-          Positive Feedback: <span>{positivePercentage}%</span>
-        </h3>
-      </>
-    );
-  }
+    render() {
+        state = {
+            good: 0,
+            neutral: 0,
+            bad: 0,
+            positivePercentage: 0,
+        };
+        return (
+            <>
+                <h1>Please leave feedback </h1>
+                <button
+                    type="button"
+                    onClick={() => {
+                        onLeaveFeedback;
+                    }}
+                >
+                    Good: <span>{good}</span>
+                </button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        onLeaveFeedback;
+                    }}
+                >
+                    Bad: <span>{bad}</span>
+                </button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        onLeaveFeedback;
+                    }}
+                >
+                    Neutral: <span>{neutral}</span>
+                </button>
+                <h2>Statistics</h2>
+                <h3>
+                    Good: <span>{good}</span>
+                </h3>
+                <h3>
+                    Bad: <span>{bad}</span>
+                </h3>
+                <h3>
+                    Neutral: <span>{neutral}</span>
+                </h3>
+                <h3>
+                    Positive Feedback: <span>{positivePercentage}%</span>
+                </h3>
+            </>
+        );
+    }
 }
 Statistics.propTypes = {
-  good: propTypes.number,
-  bad: propTypes.number,
-  neutral: propTypes.number,
-  total: propTypes.number,
-  positivePercentage: propTypes.number,
+    good: propTypes.number,
+    bad: propTypes.number,
+    neutral: propTypes.number,
+    total: propTypes.number,
+    positivePercentage: propTypes.number,
 };
 export default Statistics;
-// countTotalFeedback();
-// countPositiveFeedbackPercentage();
